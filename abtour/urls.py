4b0f965d1cf766334b10b349b1bf6f63819ea393
euqimor <romique@gmail.com>
2017-11-08 11:45:14 +0300

    robots

--- abtour/urls.py
+++ abtour/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -19,6 +21,8 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
     url('^sitemap\.xml$', sitemap),
+
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
